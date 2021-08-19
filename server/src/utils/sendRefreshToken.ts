3d2cb4b640a0aae44438ce76a0038018405391e6
jvogit <justinxvopro@gmail.com>
2021-08-19 00:47:24 -0700

    same site none cookie

--- server/src/utils/sendRefreshToken.ts
+++ server/src/utils/sendRefreshToken.ts
@@ -5,6 +5,11 @@ export const sendRefreshToken = (res: Response, token: string) => {
     {
       httpOnly: true,
       path: "/refresh_token",
+      // allows cross site cookies to be sent (different domain names)
+      // must allow third party cookies in browser
+      // I believe not necessary if you are going to use one domain name
+      sameSite: "none",
+      secure: true
     }
   );
 }
